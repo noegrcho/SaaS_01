8c46ba49fcd772da7756fca42075d9114ab72e81
noegrancho <noegrancho@gmail.com>
2026-07-03 16:02:08 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Permet les requêtes sortantes vers SerpAPI depuis les Server Functions
-  experimental: {},
-};
-
-module.exports = nextConfig;
